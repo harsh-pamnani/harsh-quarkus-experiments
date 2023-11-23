48842be3535bcc5de6e4c72469199ec89b3f93a3
Harsh Pamnani <harsh.pamnani@justeattakeaway.com>
2023-11-23 10:12:00 -0700

    Just added some comments

--- src/main/java/main/jacksonandlombok/PersonMain.java
+++ src/main/java/main/jacksonandlombok/PersonMain.java
@@ -7,11 +7,11 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 public class PersonMain {
     public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new Jdk8Module());
         Person1 p1 = mapper.readValue("{\"name\": \"harsh\"}", Person1.class);
-        System.out.println(p1);
+        System.out.println(p1); // Person1 has @Builder.Default for age. So, it will generate empty Optional
+
+        mapper.registerModule(new Jdk8Module());
         Person2 p2 = mapper.readValue("{\"name\": \"harsh\"}", Person2.class);
-        System.out.println(p2);
-        System.out.println(p2.age().orElse(5));
+        System.out.println(p2);// Person2 is a record. So, by default it will generate `null` for age. but we have registered Jdk8Module. So, it will generate empty Optional
     }
 }
