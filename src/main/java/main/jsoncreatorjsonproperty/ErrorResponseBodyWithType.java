e69f538fa6c95ec1b1efa3eee3b009a500245cb2
Harsh Pamnani <harsh.pamnani@justeattakeaway.com>
2024-03-25 10:56:09 -0600

    Added code for Jackson experiments

--- src/main/java/main/jsoncreatorjsonproperty/ErrorResponseBodyWithType.java
+++ src/main/java/main/jsoncreatorjsonproperty/ErrorResponseBodyWithType.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@JsonInclude(Include.NON_NULL)
+import java.util.Optional;
+
+@JsonInclude(Include.NON_ABSENT)
 @RegisterForReflection
-public record ErrorResponseBodyWithType(String error, String message, String type) {
+public record ErrorResponseBodyWithType(String error, String message, String type, Optional<Integer> discount) {
 }
--- src/main/java/main/jsoncreatorjsonproperty/JsonMain.java
+++ src/main/java/main/jsoncreatorjsonproperty/JsonMain.java
@@ -1,6 +1,9 @@
 package main.jsoncreatorjsonproperty;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
+import java.util.Optional;
 
 public class JsonMain {
     public static void main(String[] args) {
@@ -8,7 +11,7 @@ public class JsonMain {
         String json = "{\"id\":1,\"name\":\"Harsh\"}";
         String jsonWithDifferentFieldName = "{\"id\":2,\"firstName\":\"Jai\"}";
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new Jdk8Module());
 
         try {
             Employee1 e1 = objectMapper.readValue(json, Employee1.class);
@@ -22,10 +25,11 @@ public class JsonMain {
             EmployeeWithCreator e3 = objectMapper.readValue(jsonWithDifferentFieldName, EmployeeWithCreator.class);
             System.out.println(e3);
 
-            ErrorResponseBodyWithType ex1 = new ErrorResponseBodyWithType("AAA", "something went wrong 1", "TYPE_1");
-            ErrorResponseBodyWithType ex2 = new ErrorResponseBodyWithType("BBB", "something went wrong 2", null);
+            ErrorResponseBodyWithType ex1 = new ErrorResponseBodyWithType("AAA", "something went wrong 1", "TYPE_1", Optional.of(1));
+            ErrorResponseBodyWithType ex2 = new ErrorResponseBodyWithType("BBB", "something went wrong 2", null, Optional.ofNullable(null));
 
-            // Since "ex2" has `type` as null, and we have set "@JsonInclude(Include.NON_NULL)" for the class, it will have only "error" and "message" in the JSON.
+            // Since "ex2" has `type` as null. So if we have set "@JsonInclude(Include.NON_NULL)" for the class, it will have only "error" and "message" in the JSON.
+            // Also, "ex2" has `discount` as optional null, and we have set "@JsonInclude(Include.NON_ABSENT)" for the class, it will have only "error" and "message" in the JSON.
             System.out.println(objectMapper.writeValueAsString(ex1));
             System.out.println(objectMapper.writeValueAsString(ex2));
         } catch (Exception e) {
