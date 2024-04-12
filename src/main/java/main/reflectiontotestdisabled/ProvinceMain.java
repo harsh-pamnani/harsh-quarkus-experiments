b5b6291d1f96bd03cc87d8437278e34b104736c8
Harsh Pamnani <harsh.pamnani@justeattakeaway.com>
2024-04-12 10:54:14 -0600

    Added reflection experiments with Jackson

--- src/main/java/main/reflectiontotestdisabled/ProvinceMain.java
+++ src/main/java/main/reflectiontotestdisabled/ProvinceMain.java
@@ -2,13 +2,22 @@ package main.reflectiontotestdisabled;
 
 import io.quarkus.runtime.annotations.QuarkusMain;
 
+/*
+    IMPORTANT
+    Uncomment @QuarkusMain from below and comment it everywhere else in the project because there can be only one main in Quarkus
+
+    1. Generate the native-image using "native" profile (just run clean install with native profile)
+    2. That will generate the native image in target folder
+    3. Run the native image using "./target/reflection-1.0.0-SNAPSHOT-runner main.reflection.Exp2"
+    4. Since Province is not registered for reflection, it will return `false` even for deprecated provinces (i.e. NY)
+    5. Just add @RegisterForReflection in Province and run the native image again to see the results. It should be `true` now.
+ */
 //@QuarkusMain
 public class ProvinceMain {
     public static void main(String[] args) {
         System.out.println(Province.isDeprecated("ON"));
 
-        // Since Province is not registered for reflection, it will return `false` for deprecated provinces (i.e. NY)
-        // Just uncomment @RegisterForReflection from Province and run the native image again to see the results. It should be `true` now.
+        // Expected: true, but Actual: false
         System.out.println(Province.isDeprecated("NY"));
     }
 }
