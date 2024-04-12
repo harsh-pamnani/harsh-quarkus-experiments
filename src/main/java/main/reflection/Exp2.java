853d13a61e6533744d379ee6dc19c6e34ccad250
Harsh Pamnani <harsh.pamnani@justeattakeaway.com>
2024-04-12 11:06:31 -0600

    Removed RegisterForReflection from Exp2

--- src/main/java/main/reflection/Exp2.java
+++ src/main/java/main/reflection/Exp2.java
@@ -1,8 +1,5 @@
 package main.reflection;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection
 public record Exp2(String name) {
     public static String greetings() {
         return "Hello from Exp2";
