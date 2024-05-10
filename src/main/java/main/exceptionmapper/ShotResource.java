5e4a6fdd7a077f7b5f6c58bfebfdd5a92c19f757
Harsh Pamnani <harsh.pamnani@justeattakeaway.com>
2024-05-09 21:37:03 -0600

    Added open api dependency and resolved the native image build issues

--- src/main/java/main/exceptionmapper/ShotResource.java
+++ src/main/java/main/exceptionmapper/ShotResource.java
@@ -5,8 +5,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 /**
- * IMPORTANT - For now, it doesn't work with native image build due to some GraalVM issues.
- * So, enable "quarkus-rest-jackson" and run Quarkus application in IntelliJ IDEA to see the output of exception mappers.
  * Hit `POST http://localhost:8080/shot` endpoint with body as `{"rowNumber": -1}`.
  * See that it generates exception with `406` error - utilizing the customer exception mapper.
  */
