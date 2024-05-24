933f45aed99f9d8c9df336574477b1b70535c4c8
Harsh Pamnani <harsh.pamnani@justeattakeaway.com>
2024-05-24 10:26:40 -0600

    Added config mapping

---
+++ src/main/java/main/configsource/AppConfig.java
@@ -0,0 +1,28 @@
+package main.configsource;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+/**
+ * Reference https://quarkus.io/guides/config-reference#environment-variables
+ *
+ * PART-1
+ * The @ConfigMapping annotation support the following naming strategies:
+ * KEBAB_CASE (default) - The method name is derived by replacing case changes with a dash to map the configuration property.
+ * VERBATIM - The method name is used as is to map the configuration property.
+ * SNAKE_CASE - The method name is derived by replacing case changes with an underscore to map the configuration property.
+ *
+ * PART-2
+ * Environment variables names follow the conversion rules specified by MicroProfile Config. Config searches three environment variables for a given property name (e.g. foo.BAR.baz):
+ * foo.BAR.baz - Exact match
+ * foo_BAR_baz - Replace each character that is neither alphanumeric nor _ with _
+ * FOO_BAR_BAZ - Replace each character that is neither alphanumeric nor _ with _; then convert the name to upper case
+ */
+@ConfigMapping(prefix = "xyz")
+public interface AppConfig {
+
+    // Since we have added `apiKey` as method name in java, it will look for `xyz.api-key` property as per naming above in PART-1
+    // If it is in application.properties, it should be exact match - `xyz.api-key`. No alterations allowed.
+    // If not, based on Part-2 documentation, first it will look for `xyz.api-key` env, then `xyz_api_key` env, and then `XYZ_API_KEY` env
+    String apiKey();
+}
--- src/main/java/main/configsource/ConfigResource.java
+++ src/main/java/main/configsource/ConfigResource.java
@@ -1,9 +1,19 @@
 package main.configsource;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @Path("/config")
 public class ConfigResource {
@@ -18,6 +28,9 @@ public class ConfigResource {
     @ConfigProperty(name = "SECRET")
     String secretProperty;
 
+    @Inject
+    AppConfig appConfig;
+
     @GET
     @Path("/print-prop-values")
     public String getMyPropValue() {
@@ -27,9 +40,25 @@ public class ConfigResource {
         // propB's value from custom config will be ignored because it has ordinal 220 (vs 250 for application properties)
         LOGGER.infov("`my.propB` value : {0}", customPropertyB);
 
-       // SECRET from application.properties will be ignored app because will be taken from .env (as it has precedence over app props)
+        // SECRET from application.properties will be ignored app because will be taken from .env (as it has precedence over app props)
         LOGGER.infov("`SECRET` value : {0}", secretProperty);
 
+        // xyz!api%key from env gets translated to `xyz.api-key` (because of ConfigMapping) and `xyz_api_key` for config properties. Print all to check
+        LOGGER.infov("`apiKey` value : {0}", appConfig.apiKey());
+
         return "Check console for all properties";
+    }
+
+    private static void printAllProperties() {
+        Config config = ConfigProvider.getConfig();
+
+        Iterable<String> propertyNames = config.getPropertyNames();
+        List<String> allProperties = StreamSupport.stream(propertyNames.spliterator(), false).toList();
+        for (String prop: allProperties) {
+            LOGGER.info("UNIQUE LOG : " + prop);
+            if (prop.toLowerCase().startsWith("xyz")) {
+                LOGGER.info("UNIQUE LOG xyz : " + config.getConfigValue(prop));
+            }
+        }
     }
 }
