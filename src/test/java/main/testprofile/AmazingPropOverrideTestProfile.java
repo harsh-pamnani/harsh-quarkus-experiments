a5ba4c5b57c88ebc843c397a404b0f88397027a0
Harsh Pamnani <harsh.pamnani@justeattakeaway.com>
2025-01-03 11:05:02 -0700

    Added QuarkusTestProfile changes

---
+++ src/test/java/main/testprofile/AmazingPropOverrideTestProfile.java
@@ -0,0 +1,12 @@
+package main.testprofile;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+
+import java.util.Map;
+
+public class AmazingPropOverrideTestProfile implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+        return Map.of("amazingProp", "New overridden values from test profile");
+    }
+}
---
+++ src/test/java/main/testprofile/QuarkusTestProfileTest.java
@@ -0,0 +1,40 @@
+package main.testprofile;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import lombok.extern.jbosslog.JBossLog;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.Test;
+
+@JBossLog
+@QuarkusTest
+class QuarkusTestProfileTest {
+    @ConfigProperty(name="amazingProp")
+    String amazingProp;
+
+    @ConfigProperty(name="awesomeProp")
+    String awesomeProp;
+
+    @Test
+    void testWithoutOverride() {
+        log.info("HP: amazingProp without override: " + amazingProp);
+        log.info("HP: awesomeProp without override: " + awesomeProp);
+    }
+}
+
+@TestProfile(AmazingPropOverrideTestProfile.class)
+@JBossLog
+@QuarkusTest
+class QuarkusTestProfileWithOverrideTest {
+    @ConfigProperty(name="amazingProp")
+    String amazingProp;
+
+    @ConfigProperty(name="awesomeProp")
+    String awesomeProp;
+
+    @Test
+    void testWithOverride() {
+        log.info("HP: amazingProp with override: " + amazingProp); // Value is overridden by test profile
+        log.info("HP: awesomeProp without override: " + awesomeProp); // No override in test profile. So, value will be taken from props
+    }
+}
