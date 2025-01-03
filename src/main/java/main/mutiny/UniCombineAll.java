fc806c12c74b1cd89a5a7460bf89ef0c8615f30e
Harsh Pamnani <harsh.pamnani@justeattakeaway.com>
2025-01-03 12:16:24 -0700

    Added UniCombineAll and discardItems code

---
+++ src/main/java/main/mutiny/UniCombineAll.java
@@ -0,0 +1,41 @@
+package main.mutiny;
+
+import io.smallrye.mutiny.Uni;
+import lombok.extern.jbosslog.JBossLog;
+
+@JBossLog
+public class UniCombineAll {
+    public static void main(String[] args) {
+        Uni<String> uni1 = Uni.createFrom().item(() -> {
+            // Simulate a delay
+            try {
+                Thread.sleep(1000);
+                log.info("First Uni");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "Task 1";
+        });
+
+        Uni<String> uni2 = Uni.createFrom().item(() -> {
+            try {
+                Thread.sleep(5000);
+                log.info("Second Uni");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "Task 2";
+        });
+
+        Uni<String> combinedUni = Uni.combine()
+                                     .all()
+                                     .unis(uni1, uni2)
+                                     .asTuple()
+                                     .map(tuple -> tuple.getItem1() + " " + tuple.getItem2());
+
+        // combinedUni result executed only after both Uni completed
+        log.info("Uni execution started");
+        combinedUni.subscribe().with(log::info);
+        log.info("Uni execution ended");
+    }
+}
---
+++ src/main/java/main/mutiny/UniCombineAllWithFailure.java
@@ -0,0 +1,40 @@
+package main.mutiny;
+
+import io.smallrye.mutiny.Uni;
+import lombok.extern.jbosslog.JBossLog;
+
+@JBossLog
+public class UniCombineAllWithFailure {
+    public static void main(String[] args) {
+        Uni<String> uni1 = Uni.createFrom().item(() -> {
+            // Simulate a delay
+            try {
+                Thread.sleep(1000);
+                throw new RuntimeException("Task 1 failed");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Uni<String> uni2 = Uni.createFrom().item(() -> {
+            try {
+                Thread.sleep(5000);
+                log.info("Second Uni");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "Task 2";
+        });
+
+        // discardItems() is used to discard all emitted items and allow the stream to complete without processing any of the items.
+        Uni<Void> combinedUni = Uni.combine()
+                                     .all()
+                                     .unis(uni1, uni2)
+                                     .discardItems();
+
+        // If one of the Uni instances fails, combinedUni will fail immediately with the same failure.
+        log.info("Uni execution started");
+        combinedUni.subscribe().with(log::info);
+        log.info("Uni execution ended");
+    }
+}
